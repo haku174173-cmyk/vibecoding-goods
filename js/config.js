d0d45f7448b6ff17e8c8ad9cacdac704274bd484
박성은 <parksungeun@bagseong-eun-ui-MacBookPro.local>
2026-06-05 19:20:32 +0900

    Set Toss test client key

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://oyrdgyemgzlgrjppdswb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95cmRneWVtZ3psZ3JqcHBkc3diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NDIzMDQsImV4cCI6MjA5NjIxODMwNH0.UCsV-vuQF8MgL0QL1PgpklVb8kV7YSTasBJ7F4X6Bjk";
-const TOSS_CLIENT_KEY = "test_ck_placeholder"; // 토스 테스트 클라이언트 키로 교체 필요
+const TOSS_CLIENT_KEY = "test_ck_BX7zk2yd8yn5mKaDDD6v3x9POLqK";
